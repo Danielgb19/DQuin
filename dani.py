555787b9bcc633bc8b42bf2cbc45d86a7b4b55a5
DaniQ <danieleliasquintanalobos@gmail.com>
2025-04-15 13:29:26 -0400

    clase de pyton

--- dani.py
+++ dani.py
@@ -1,15 +1,63 @@
+# clase de pyton
+
 #declaracion de variable
-nombre="DANIEL"
-edad=18
-#ejemplo de concatenacion
-print("hola", nombre,"y su edad es", edad)
+# nombre="DANIEL"
+# edad=18
+# #ejemplo de concatenacion
+# print("hola", nombre,"y su edad es", edad)
 
 # n1=9
 # n2=20
 # print(n1+n2)
 
-
-
+#suma de dos numeros
 # n1=int(input("ingrese un numero "))
 # n2=int(input("ingrese un numero "))
 # print("el resultado de la suma es ", n1+n2)
+
+
+# sacar promedio 3 numeros 
+
+# print("ingrese 3 numero para sacar su promedio ")
+# n1=int(input("ingrese un numero "))
+# n2=int(input("ingrese un numero "))
+# n3=int(input("ingrese un numero "))
+# prom=(n1+n2+n3)/3
+# print("el promedio es ", prom)
+# if prom>=40:
+#     print("el alumno aprobo")
+# else:
+#     print("el alumno reprobo")
+
+
+#si es mayor o menor 
+
+# edad=int(input("ingrese su edad "))
+# if edad>=18:
+#     print("es mayor de edad")
+# else:
+#     print("es menor de edad")
+
+
+#ingrese 3 numeros y muestre el mayor
+
+# n1=int(input("ingrese un numero "))
+# n2=int(input("ingrese otro numero "))
+# n3=int(input("ingrese otro numero "))
+
+# if n1>n2 and n1>n3:
+#     print("el numero es mayor ", n1, "es mayor ")
+# elif n2>n3:
+#     print("el numero ", n2, "es mayor ")
+# else:
+#      print("ekl numero ", n3, "el numeor es mayor")
+
+
+#par impar 
+# n1=int(input("ingrese un numero:  "))
+
+# if n1 % 2==0:
+#    print("el numero es par ")
+# else:
+#    print("el numero es impar")
+   
