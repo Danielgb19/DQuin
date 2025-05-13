5eec212c41c63e5f048acad5df6ac0cd89da38ff
daniel <danieleliasquintanalobos@gmail.com>
2025-05-13 12:20:24 -0400

    nuevo file

---
+++ match.py
@@ -0,0 +1,64 @@
+
+
+
+# def suma2 (n1,n2):
+#     print(" el resultado de la suma es : -->", n1+n2) 
+
+# def suma ():
+#     n1=int(input("ingrese un numero "))
+#     n2=int(input("ingrese otro numero "))
+#     print(" el resultado de la suma es : -->", n1+n2) 
+
+# def resta ():
+#     n1=int(input("ingrese un numero "))
+#     n2=int(input("ingrese otro numero "))
+#     print(" el resultado de la resta es : -->", n1-n2) 
+
+# def multiplicar ():
+#     n1=int(input("ingrese un numero "))
+#     n2=int(input("ingrese otro numero "))
+#     print(" el resultado de la multiplicaion es : -->", n1*n2) 
+
+# def division ():
+#     n1=int(input("ingrese un numero "))
+#     n2=int(input("ingrese otro numero "))
+#     print(" el resultado de la division es : -->", n1/n2) 
+
+
+
+# def calcu():
+#     while True:
+
+#         op=int(input (''' ingrese una opcion
+#         suma (1)
+#         resta (2)
+#         multiplicar (3)
+#         division (4)
+#         salir (5)
+#         '''))
+
+#         match op: 
+#             case 1 :
+#                 print("suma ")
+#                 suma()
+#             case 2 :
+#                 print("resta")
+#                 resta()
+#             case 3 :
+#                 print("multiplicar")
+#                 multiplicar()
+#             case 4 :
+#                 print("division")
+#                 division()
+#             case 5:
+#                 print ("salir ")
+#                 break
+
+#             case _:
+#                 print("opcionn invalida")
+
+# # calcu()
+# suma2(7,9)
+
+## nuevo menu
+
